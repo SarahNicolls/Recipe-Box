9fd7b718ba539444f758de2a3ae1195830e585c2
Mariah Grey <mariah.l.grey@gmail.com>
2017-11-09 15:36:37 -0800

    added optional auth fields in routes

--- api/routes/user/getAll.js
+++ api/routes/user/getAll.js
@@ -2,6 +2,7 @@ module.exports = {
   method: "GET",
   path: "/api/users",
   config: {
+    auth: { mode: "optional" },
     handler: function(request, reply) {
       this.models.User
         .filter({})
