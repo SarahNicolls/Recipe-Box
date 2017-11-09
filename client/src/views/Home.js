91fb90997b266342ca7775bbc43a4ed99915168b
Mariah Grey <mariah.l.grey@gmail.com>
2017-11-09 15:55:11 -0800

    home remove field

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import api from "../api";
 
 class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      recipe: {}
+    };
+  }
+
+  componentDidMount() {
+    api.recipes.getAll().then(data => {
+      this.setState(state => {
+        return {
+          recipe: data
+        };
+      });
+    });
+  }
+
   render() {
+    console.log(this.state);
     return (
       <div>
         <div>
