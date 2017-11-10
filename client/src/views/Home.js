06bdfc19fdce056e5898ebfbf64cbba111b85659
Will <Will@toph.lan>
2017-11-10 09:24:10 -0800

    started styling

--- client/src/views/Home.js
+++ client/src/views/Home.js
@@ -27,35 +27,68 @@ class Home extends Component {
       <div>
         <div>
           <div>
-            <Link to={"/Signup"}>Signup</Link>
+            <SLink to={"/Signup"}>Signup</SLink>
           </div>
           <div>
-            <Link to={"/Login"}>Login</Link>
+            <SLink to={"/Login"}>Login</SLink>
           </div>
-          <h1>Recipe Box</h1>
+          <Title>Recipe Box</Title>
         </div>
-
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
-            <Link to={`/recipes/${recipe.id}`}>
-              <Meal>{recipe.strMeal}</Meal>
-              <Image src={recipe.strMealThumb} alt={recipe.strMeal} />
-            </Link>
-          </div>
-        ))}
-
+        <Recipes>
+          {recipes.map(recipe => (
+            <Recipe key={recipe.id}>
+              <Link to={`/recipes/${recipe.id}`}>
+                <Meal>{recipe.strMeal}</Meal>
+                <Image src={recipe.strMealThumb} alt={recipe.strMeal} />
+              </Link>
+            </Recipe>
+          ))}
+        </Recipes>
       </div>
     );
   }
 }
 
+const Title = styled.h1.attrs({ className: "f1" }) `
+  text-align: center;
+`;
+
+const SLink = styled(Link) `
+  text-decoration: none;
+`;
+
 const Meal = styled.div`
   font-size: 24px;
+  width: 250px;
+  min-height: 50px;
+`;
+
+const Recipes = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+`;
+
+const Recipe = styled.div`
+  width: calc(100vw / 3);
+  display: flex;
+  justify-content: space-evenly;
+
+  &:hover {
+    border: 1px solid #ddd;
+    box-shadow: 2px 2px 4px 2px rgba( 0, 0, 0, 0.2 );
+  }
+
+  > a {
+    text-decoration: none;
+  }
 `;
 
 const Image = styled.img`
   height: 150px;
   border-radius: 50%;
+  padding: 15px;
 `;
 
 
